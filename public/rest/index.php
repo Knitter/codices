5638411d35c3184aee3c61e35041232e72d7cf6d
Sérgio Lopes <knitter.is@gmail.com>
2016-10-07 09:12:47 +0100

    QFIX: Fixes typo due to wrong copy+paste

--- public/rest/index.php
+++ public/rest/index.php
@@ -8,7 +8,8 @@ if (is_file(realpath(__DIR__ . '/../../vagrant.config.lock'))) {
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../../aval/common/config/bootstrap.php');
+require(__DIR__ . '/../../codices/common/config/bootstrap.php');
 
-$config = yii\helpers\ArrayHelper::merge(require(__DIR__ . '/../../aval/common/config/main.php'), require(__DIR__ . '/../../aval/private/config/main.php'));
+$config = yii\helpers\ArrayHelper::merge(require(__DIR__ . '/../../codices/common/config/main.php')
+        , require(__DIR__ . '/../../codices/rest/config/main.php'));
 (new yii\web\Application($config))->run();

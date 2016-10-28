1523640e355805f97c5357f87243ae8919da3a56
Sérgio Lopes <knitter.is@gmail.com>
2016-10-28 11:17:23 +0100

    Adds data folder for upladed author photos and cover images. Fixes missing @app alias in boostrap file.

--- codices/common/config/bootstrap.php
+++ codices/common/config/bootstrap.php
@@ -5,3 +5,4 @@ Yii::setAlias('@container', realpath(__DIR__ . '/../../'));
 Yii::setAlias('@common', realpath(__DIR__ . '/../../common'));
 Yii::setAlias('@console', realpath(__DIR__ . '/../../console'));
 Yii::setAlias('@rest', realpath(__DIR__ . '/../../rest'));
+Yii::setAlias('@app', realpath(__DIR__ . '/../../app'));

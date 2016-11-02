ad1da0763745c78ac7e0101f1964d661df3e66d6
Sérgio Lopes <knitter.is@gmail.com>
2016-11-02 17:34:44 +0000

    Fixes debug mode detection and updates dependencies.

--- public/app/index.php
+++ public/app/index.php
@@ -1,14 +1,10 @@
 <?php
 
-//NOTE: Reverting to simple flag setting for now
-//if (is_file(realpath(__DIR__ . '/../../vagrant.config.lock'))) {
-//
-//    defined('YII_DEBUG') or define('YII_DEBUG', true);
-//    defined('YII_ENV') or define('YII_ENV', 'dev');
-//}
+if (is_file(realpath(__DIR__ . '/../../vagrant.config.lock'))) {
 
-define('YII_DEBUG', true);
-define('YII_ENV', 'dev');
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');

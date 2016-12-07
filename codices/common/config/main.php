b97d83feefd8d8db0dec4349c8404c046f5cc8f0
Sérgio Lopes <knitter.is@gmail.com>
2016-12-07 15:37:08 +0000

    QFIX dashboard bug and version update

--- codices/common/config/main.php
+++ codices/common/config/main.php
@@ -4,7 +4,7 @@ $db = require(__DIR__ . '/db.php');
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'version' => '1.3.0',
+    'version' => '1.4.1',
     'vendorPath' => __DIR__ . '/../../../vendor',
     'components' => [
         'cache' => ['class' => 'yii\caching\FileCache'],

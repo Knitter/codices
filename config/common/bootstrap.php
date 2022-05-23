581b468898940bae27c77a8e41b5bd84ba4363e5
Sérgio Lopes <knitter.is@gmail.com>
2022-05-24 00:59:59 +0100

    REFACTORING Removes remaining test and config files

--- config/common/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$parent = dirname(dirname(__DIR__));
-
-Yii::setAlias('@common', $parent . '/common');
-Yii::setAlias('@codices', $parent . '/codices');
-Yii::setAlias('@console', $parent . '/console');
